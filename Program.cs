cb1f2d23c1a15c7044a755dbbf06c15f31025149
aakif <aakif@DESKTOP-NCDLL1I>
2023-07-03 21:52:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Deliverable_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            string series = "";
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter an integer number between 1 and 100: ");
+                    input = int.Parse(Console.ReadLine());
+
+                    if (input >= 1 && input <= 100)
+                    {
+                        break;
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer value");
+                }
+            }
+            
+            while (true)
+            {
+                    Console.Write("Specify the series type: Even or Odd: ");
+                    series = Console.ReadLine();
+
+                    if (series == "Even" || series == "Odd")
+                    {
+                        break;
+                    }
+            }
+
+            if (series == "Odd")
+            {
+                Console.WriteLine();
+                Console.WriteLine("You have selected the " + series + " series. The numbers between 0 and " + input + " are:");
+                for (int i = 1; i <= input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+            if (series == "Even")
+            {
+                Console.WriteLine();
+                Console.WriteLine("You have selected the " + series + " series. The numbers between 0 and " + input + " are:");
+                for (int i = 0; i <= input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+
+    }
+}
